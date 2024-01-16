276545b04f29dd7fe99d36d0ca24694ec6339f9b
marialuiza <mluizacmarques@gmail.com>
2024-01-16 18:23:08 +0000

    ex01 main

---
+++ Module_00/ex01/main.cpp
@@ -0,0 +1,50 @@
+#include "phonebook.hpp"
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	Phonebook	phonebook;
+	std::string	command;
+	int			size = 0;
+	int			position = 0;
+
+	if (argc == 1)
+	{
+		while (1)
+		{
+			std::cout << "Chose your command: " << std::endl;
+			getline(std::cin, command);
+			if (position == 8)
+			{
+				position = 0;
+			}
+			if (command == "ADD")
+			{
+				system("clear");
+				std::cout << "Adding a new contact:" << std::endl << std::endl;
+				phonebook.addContact(position);
+				if (size < 8)
+					size++;
+				position++;
+			}
+			else if (command == "SEARCH")
+			{
+				system("clear");
+				phonebook.displayContact(size);
+				phonebook.searchDisplay(size);
+			}
+			else if (command == "EXIT")
+			{
+				system("clear");
+				std::cout << "Bye!" << std::endl;
+				break ;
+			}
+			else
+			{
+				system("clear");
+				std::cout << "Wrong command. Try again!" << std::endl << std::endl;
+			}
+		}
+	}
+	return (0);
+}
