3f03deb2040612fdac2efb7214763c4c948c9882
macastan <mluizacmarques@gmail.com>
2024-01-23 17:38:20 +0000

    ex05 done

---
+++ Module_01/ex05/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macastan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 16:57:48 by macastan          #+#    #+#             */
+/*   Updated: 2024/01/23 16:57:50 by macastan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	harl;
+	std::string	level;
+
+	std::cout << "Choose one Harl { DEBUG, INFO, WARNING, ERROR } " << std::endl << std::endl;
+	while(level.empty())
+	{
+		if (!getline(std::cin, level))
+		{
+			std::cout << "Error, try again!" << std::endl;
+			return (1);
+		}
+	}
+	std::cout << std::endl << "Here is your Harl:" << std::endl;
+	harl.complain(level);
+	return (0);
+}
